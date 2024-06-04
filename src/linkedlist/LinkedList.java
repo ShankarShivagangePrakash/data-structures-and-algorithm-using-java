a7658e9b89a3ef4e2ee5de1edb97c333fab4ba20
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-06-04 13:58:57 +0530

    linked list - append, length, empty linked list creation

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -6,6 +6,12 @@ public class LinkedList {
     private Node tail;
     private int length;
 
+    // create empty linked list
+    public LinkedList(){
+        head = tail = null;
+        length = 0;
+    }
+
     // LinkedList constructor will be called only when linkedList is created for the very first time.
     public LinkedList(int value){
 
@@ -18,16 +24,49 @@ public class LinkedList {
         length = 1;
     }
 
+    public void append(int value){
+        Node node = new Node(value);
+        // both head and tail null means linked list is empty
+        // alternatively we can check for length == 0 for empty linked list scenario.
+        if (head == null && tail == null) {
+            // If linked list is empty and current element is the first element
+            // assign both head and tail to current element.
+            head = node;
+            tail = node;
+            length = 1;
+        } else{
+            // assign tail.next to new Node so last element in the list points to the new node.
+            tail.next = node;
+            // point tail to new Node.
+            tail = node;
+            length++;
+        }
+    }
+
+    public void getHead(){
+        System.out.println("head of the linkedList: " + head);
+    }
+
+    public void getTail(){
+        System.out.println("Tail of the linked list: " + tail);
+    }
+
+    public void getLength(){
+        System.out.println("Length of the linked list: " + length);
+    }
+
+
     public void printList(){
         /* we assign the head reference to a temp variable
          keep moving till we reach end of the linkedList,
          means till temp = null */
         Node temp = head;
         while (temp != null){
-            System.out.println(temp.value);
+            System.out.print("-->" + temp.value);
             // move to next element
             temp = temp.next;
         }
+        System.out.println("\n");
     }
 
     // Inner class
--- src/linkedlist/LinkedListTest.java
+++ src/linkedlist/LinkedListTest.java
@@ -6,5 +6,10 @@ public class LinkedListTest {
 
         LinkedList linkedList = new LinkedList(5);
         linkedList.printList();
+
+        LinkedList linkedList1 = new LinkedList();
+        linkedList1.append(1);
+        linkedList1.append(2);
+        linkedList1.printList();
     }
 }
