729520d0e3f693f177e2446e340485856d9cf126
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-06-06 18:18:48 +0530

    Doubly linked list creation

---
+++ src/doublylinkedlist/DoublyLinkedList.java
@@ -0,0 +1,62 @@
+package doublylinkedlist;
+
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    DoublyLinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    DoublyLinkedList(int value){
+        Node node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+    class Node{
+        private int value;
+        private Node prev;
+        private Node next;
+
+        Node(int value){
+            this.value = value;
+        }
+
+        public void setValue(int value){
+            this.value = value;
+        }
+
+        public int getValue(){
+            return this.value;
+        }
+    }
+
+    public void getHead(){
+        System.out.println("head of the linkedList: " + head);
+    }
+
+    public void getTail(){
+        System.out.println("Tail of the linked list: " + tail);
+    }
+
+    public void getLength(){
+        System.out.println("Length of the linked list: " + length);
+    }
+
+    public void printList(){
+        Node temp = head;
+        while(temp != null){
+            System.out.printf("--> %d", temp.getValue());
+            temp = temp.next;
+        }
+        System.out.println("\n");
+    }
+
+
+}
---
+++ src/doublylinkedlist/DoublyLinkedListTest.java
@@ -0,0 +1,14 @@
+package doublylinkedlist;
+
+public class DoublyLinkedListTest {
+
+    public static void main(String[] args){
+
+        DoublyLinkedList doublyLinkedList1 = new DoublyLinkedList(1);
+        doublyLinkedList1.getHead();
+        doublyLinkedList1.getTail();
+        doublyLinkedList1.getLength();
+        doublyLinkedList1.printList();
+
+    }
+}
