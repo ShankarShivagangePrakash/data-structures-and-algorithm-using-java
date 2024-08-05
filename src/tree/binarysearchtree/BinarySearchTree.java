07762e8460559d36f0a18042329b789cfe9426c6
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-08-05 22:27:48 +0530

    empty binary tree creation

---
+++ src/tree/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,12 @@
+package tree.binarysearchtree;
+
+public class BinarySearchTree {
+
+    Node root;
+
+    class Node{
+        private int value;
+        private Node left;
+        private Node right;
+    }
+}
---
+++ src/tree/binarysearchtree/BinarySearchTreeTest.java
@@ -0,0 +1,10 @@
+package tree.binarysearchtree;
+
+public class BinarySearchTreeTest {
+
+    public static void main(String[] args){
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+        System.out.println(" Binary Search tree root node: " + binarySearchTree.root);
+    }
+}
