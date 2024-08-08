dfd622f2ec94c2766dfbcbe16d5c142c5604dee0
ShankarShivagangePrakash <s.p.shankar.na@gmail.com>
2024-08-08 21:32:19 +0530

    Graph remove edge

--- src/graph/GraphTest.java
+++ src/graph/GraphTest.java
@@ -7,8 +7,13 @@ public class GraphTest {
 
         graph.addVertex("A");
         graph.addVertex("B");
+        graph.addVertex("C");
 
         graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("C", "B");
+
+        graph.removeEdge("A", "B");
 
         graph.printGraph();
     }
